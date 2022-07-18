86fc7aeb4b97b4d84ea189c0aaac60140ccef92a
Jamal-Deallie <66758645+Jamal-Deallie@users.noreply.github.com>
2022-07-18 00:33:37 -0500

    READ Me Added

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://medusa-app-jd.herokuapp.com/api/v1',
-
+    mode: 'cors',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
